f4511e0d5009685921836544825d9c37dac41d49
Shigeki Suwa <shigeki.suwa@ztrehagem.dev>
2020-03-20 21:29:57 +0900

    fix regexp

--- src/api/login.ts
+++ src/api/login.ts
@@ -18,7 +18,7 @@ const validations: Validation<Payload>[] = [
   },
   {
     prop: 'email',
-    validate: ({ email }) => !email.match(/^[^@]+@[^@]$/),
+    validate: ({ email }) => !email.match(/^[^@]+@[^@]+$/),
     message: 'INVALID_FORMAT',
   },
   {
